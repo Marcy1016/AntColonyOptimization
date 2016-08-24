3fa1b375a31f489ce3acdb56b17f6d45c4693477
Marcy1016 <marcy1016@gmai.com>
2016-08-24 10:39:36 +0900

    以前のソースコードを実行できるように修正

--- aco_2016_up.java
+++ aco_2016_up.java
@@ -223,13 +223,14 @@ public class aco_2016_up{
 	result_task    = new int [RUN];
 	result_ant     = new int [RUN];
 	
-	//task_size    =new int[TASK_MAX][JOB];
-	//task_volume  =new int[TASK_MAX][JOB];
+	TASK 		 = new int[JOB];
+	task_size    = new int[TASK_MAX][JOB];
+	task_volume  = new int[TASK_MAX][JOB];
 	
-	//LAYER        =new int[JOB];
-	//F_TASK       =new int[LAYER_MAX+1][JOB];	
-	machine_size =new double[MACHINE];
-	speed        =new int[MACHINE];
+	LAYER        = new int[JOB];
+	F_TASK       = new int[LAYER_MAX+1][JOB];	
+	machine_size = new double[MACHINE];
+	speed        = new int[MACHINE];
 
 
 	//TASK_MAXの初期化
@@ -240,77 +241,60 @@ public class aco_2016_up{
 		switch (str)
 		{
 			case "TASK":
-				String[] temp = br2.readLine().split(",",0);
-				TASK = new int[temp.length];
-				JOB = temp.length;
-				for (job=0;job<temp.length;job++)
+				for (job=0;job<JOB;job++)
 				{
-					TASK[job] = Integer.parseInt(temp[job]);
+					TASK[job] = Integer.parseInt(br2.readLine());
 					System.out.println(TASK[job]);
-					TASK_MAX += TASK[job];/*これ大事*/
 				}
 				break;
 			case "task_size":
-			task_size = new int[TASK_MAX][JOB];
 				for (job=0;job<JOB;job++)
 				{
-					temp = br2.readLine().split(",",0);
-					task_size[job] = new int[temp.length];
 					for (task=0;task<TASK[job];task++)
 					{
-						task_size[task][job] = Integer.parseInt(temp[task]);
+						task_size[task][job] = Integer.parseInt(br2.readLine());
 						System.out.println(task_size[task][job]);
 					}
 				}
 				break;
 			case "task_volume":
-				task_volume = new int[TASK_MAX][JOB];
 				for (job=0;job<JOB;job++)
 				{
-					temp = br2.readLine().split(",",0);
-					task_volume[job] = new int[temp.length];
 					for (task=0;task<TASK[job];task++)
 					{
-						task_volume[task][job] = Integer.parseInt(temp[task]);
+						task_volume[task][job] = Integer.parseInt(br2.readLine());
 						System.out.println(task_volume[task][job]);
 					}
 				}
 				break;
 			case "LAYER":
-				LAYER = new int[JOB];
-				temp = br2.readLine().split(",",0);
 				for (job=0;job<JOB;job++)
 				{
-					LAYER[job] = Integer.parseInt(temp[job]);
+					LAYER[job] = Integer.parseInt(br2.readLine());
 					System.out.println(LAYER[job]);
 				}
 				break;
 			case "F_TASK":
-				F_TASK = new int[LAYER_MAX+1][JOB];
 				for (job=0;job<JOB;job++)
 				{
-					temp = br2.readLine().split(",",0);
-					F_TASK[job] = new int[temp.length];
 					for (layer=0;layer<LAYER_MAX;layer++)
 					{
-						F_TASK[layer][job] = Integer.parseInt(temp[task]);
+						F_TASK[layer][job] = Integer.parseInt(br2.readLine());
 						System.out.println(F_TASK[layer][job]);
 					}
 				}
 				break;
 			case "machine_size":
-				temp = br2.readLine().split(",",0);
 				for (machine=0;machine<MACHINE;machine++)
 				{
-					machine_size[machine] = Double.parseDouble(temp[machine]);
+					machine_size[machine] = Double.parseDouble(br2.readLine());
 					System.out.println(machine_size[machine]);
 				}
 				break;
 			case "speed":
-				temp = br2.readLine().split(",",0);
 				for (machine=0;machine<MACHINE;machine++)
 				{
-					speed[machine] = Integer.parseInt(temp[machine]);
+					speed[machine] = Integer.parseInt(br2.readLine());
 					System.out.println(speed[machine]);
 				}
 				break;
